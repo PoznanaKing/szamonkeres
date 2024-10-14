084144a468bc0617ebca099ca4af754c46869038
PoznanaKing <pozsgain@kkszki.hu>
2024-10-14 08:58:45 +0200

    GetID is ready to use

--- ProductController.cs
+++ ProductController.cs
@@ -11,36 +11,55 @@ using System.Threading.Tasks;
 
 namespace szamonkeres_
 {
-    
-        [Route("[controller]")]
-        [ApiController]
-        public class Controller : ControllerBase
+
+    [Route("szamonkeres")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        List<Tanulo> tanulok = new List<Tanulo>();
+        Connect conn = new Connect();
+        [HttpGet]
+        public List<Tanulo> Get()
         {
-            List<Tanulo> tanulok=new List<Tanulo>();
-            Connect conn = new Connect();
-            [HttpGet]
-            public List<Tanulo> Get() 
+            conn.Connection.Open();
+            string sqlCommand = "SELECT * FROM `szamonkeres` WHERE 1";
+            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            do
             {
-                conn.Connection.Open();
-                string sqlCommand = "SELECT * FROM `szamonkeres` WHERE 1";
-                MySqlCommand cmd = new MySqlCommand(sqlCommand,conn.Connection);
-                MySqlDataReader dr = cmd.ExecuteReader();
-                dr.Read();
-                do
-                {
-                    Tanulo temp = new Tanulo();
-                    temp.Id = dr.GetInt32(0);
-                    temp.jegy = dr.GetInt32(1);
-                    temp.Description = dr.GetString(2);
-                    temp.létrehozás_deje= dr.GetString(3);
-                    tanulok.Add(temp);
-                }while (dr.Read());
-                dr.Close();
-                conn.Connection.Close();
-                return tanulok;
-
-            }
-
+                Tanulo temp = new Tanulo();
+                temp.Id = dr.GetInt32(0);
+                temp.jegy = dr.GetInt32(1);
+                temp.Description = dr.GetString(2);
+                temp.létrehozás_deje = dr.GetString(3);
+                tanulok.Add(temp);
+            } while (dr.Read());
+            dr.Close();
+            conn.Connection.Close();
+            return tanulok;
         }
+        [HttpGet("{id}")]
+        public Tanulo Get(int id)
+        {conn.Connection.Open();
+            string sqlcom = $"SELECT * FROM `szamonkeres` WHERE `id`={id}; ";
+            MySqlCommand cmd = new MySqlCommand(sqlcom, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader(); dr.Read();
+            Tanulo temp = new Tanulo();
+            temp.Id = dr.GetInt32(0);
+            temp.jegy = dr.GetInt32(1);
+            temp.Description = dr.GetString(2);
+            temp.létrehozás_deje = dr.GetString(3);
+            return temp;
+        }
+        
     }
+}
+    
+    
+           
+        
+
+    
+    
 
